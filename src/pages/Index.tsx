90ce7cdd1545cb489c93dab32b3afcbdce44258e
wilfredo <will.garcia.perez@gmail.com>
2026-01-27 10:15:08 -0400

    correcion en dispositivos moviles horizontal

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/landing/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-x-hidden">
       <Header />
       <HeroSection />
       <FeaturesSection />
